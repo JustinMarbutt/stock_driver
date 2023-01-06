1132ad8b8b1330b37a89ac6df1e29ec987bfb47e
Justin Marbutt <justinmarbutt@gmail.com>
2023-01-06 01:31:27 -0600

    addin checkbox to disclaimer

--- app/js/index.js
+++ app/js/index.js
@@ -6,13 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../styles/style.scss';
 
+// Requrie tos accept for initNewGame an registerActions
 $('#termsModal').modal({backdrop: 'static', keyboard: false});
 $('#agreeButton').on('click', function() {
   initNewGame();
   registerActions();
-});
-
-$(document).ready(function() {
-  // initNewGame();
-  // registerActions();
 });
